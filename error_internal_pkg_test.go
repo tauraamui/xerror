1232cbf7e3b32ff55c5a57db8c97e29e975cef64
tauraamui <adampstringer@protonmail.com>
2023-03-22 12:28:38 +0000

    test: define further tests to ensure wrapped arg found correctly

--- error_internal_pkg_test.go
+++ error_internal_pkg_test.go
@@ -1,7 +1,31 @@
 package xerror
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/matryer/is"
+)
 
 func TestWrappedArgs(t *testing.T) {
-	wrappedArgs("wrapped err: %w")
+	is := is.New(t)
+
+	args := wrappedArgs("wrapped err: %w")
+	is.True(len(args) == 1)
+	is.Equal(args[0], 1)
+}
+
+func TestWrappedArgsAfterOtherArgs(t *testing.T) {
+	is := is.New(t)
+
+	args := wrappedArgs("too many buckets: %d/%d, wrapped err: %w")
+	is.True(len(args) == 1)
+	is.Equal(args[0], 3)
+}
+
+func TestWrappedArgsBetweenOtherArgs(t *testing.T) {
+	is := is.New(t)
+
+	args := wrappedArgs("service proc: %s failed, wrapped err: %w, lost %d")
+	is.True(len(args) == 1)
+	is.Equal(args[0], 2)
 }
